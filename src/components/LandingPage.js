5a343cc2902537e5386d3da6f842644c8e3b7c0b
godwin olele <godwinolele10@gmail.com>
2021-12-26 09:53:05 +0000

    added css carousal

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,11 +13,40 @@ const LandingPage = () => {
                     <div className="z1">
                        
                         <h1 className="p1">MusiqBanq offers <span>ZERO</span> interest <span>LOAN</span> to upcoming artistes and small business owners</h1>
-                        <p className="p2">Get yours today with ZERO <span>collateral.</span></p>
+                        <div className="secLineText">
+                        <div className="zero">Get yours today with ZERO</div>
+                        
+                        <div className="content-slider">
+                          <div className="slider">
+                            <div className="mask">
+                               <ul>
+                                      <li className="anim1">
+                     <div className="quote">Collateral</div>
+        </li>
+        <li className="anim2">
+          <div className="quote">Bank statement</div>
+        </li>
+        <li className="anim3">
+          <div className="quote">Guarantor</div>
+        </li>
+        <li className="anim4">
+          <div className="quote">Bvn Needed</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </div>
+                        </div>
+
                         <Link exact to="/SignUp" className="signUpButton">Sign up</Link>
                     </div>
-                    <div className="z2"></div>
-                    <div className="z3"></div>
+                    <div className="z2">
+                        <div className="pattern1"></div>
+                        <div className="pattern2"></div>
+                    </div>
+                    <div className="z3">
+                    <div className="pattern3"></div>
+                    </div>
                 </div>
             </div>
         </>
