8a24fd77e649de03fcd16c038c7533cb995a9944
godwin olele <godwinolele10@gmail.com>
2022-02-09 14:08:17 +0000

    user page album card

--- src/components/userPage.js
+++ src/components/userPage.js
@@ -19,10 +19,16 @@ const userPage = () => {
             <div className="albumNameAndBtns">
              <div className="albumName">
                 <div className="name">Central Cee</div>
-                <div className="title">welcome to brixton</div>
+                <div className="title">Loading</div>
              </div>
-             <div className="playAndDownloadBtn"></div>
-             <div className="addToChartBtn"></div>
+             <div className="playAndDownloadBtn">
+               <div className="play"><img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/20/000000/external-play-call-to-action-bearicons-outline-color-bearicons.png" alt=''/></div>
+               <div className="download">
+                 <img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/20/000000/external-download-call-to-action-bearicons-outline-color-bearicons.png" alt=""/>
+               </div>
+               <div className="moreInfo"><img src="https://img.icons8.com/cotton/20/000000/info--v3.png" alt=""/></div>
+             </div>
+             <button className="addToChartBtn">Add to chart</button>
             </div>
           </div>
           <div className="album2"></div>
